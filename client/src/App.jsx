727aeba29744766e37cb03e7cf5f9e5b5950964f
Naman Soni <namantsoni@gmail.com>
2024-05-11 11:45:30 +0530

    Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import SignIn from './pages/SignIn.jsx';
 import About from './pages/About.jsx';
 import SignUp from './pages/SignUp.jsx';
 import Profile from './pages/Profile.jsx';
+import Header from './components/Header.jsx';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/about' element={<About/>} />
