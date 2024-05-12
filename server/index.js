2a95d2966a03e74a8acbe7a371f38e49cba50ce5
Naman Soni <namantsoni@gmail.com>
2024-05-12 12:30:56 +0530

    Error Handler, Sign Up UI and functionality

--- server/index.js
+++ server/index.js
@@ -15,8 +15,19 @@ const app = express();
 
 app.use(express.json());
 
+//Routes 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+//Error middleware
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
