4af27f59e77460321f0ac8921674e288135cbdff
Naman Soni <namantsoni@gmail.com>
2024-05-13 23:20:48 +0530

    Updated header (private profile route) and Profile page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import About from './pages/About.jsx';
 import SignUp from './pages/SignUp.jsx';
 import Profile from './pages/Profile.jsx';
 import Header from './components/Header.jsx';
+import PrivateRoute from './components/PrivateRoute.jsx';
 
 export default function App() {
   return (
@@ -16,7 +17,9 @@ export default function App() {
         <Route path='/about' element={<About/>} />
         <Route path='/sign-in' element={<SignIn/>} />
         <Route path='/sign-up' element={<SignUp/>} />
-        <Route path='/profile' element={<Profile/>} />
+        <Route element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile/>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
