111d91a4ca4f6d9afdc7ef11d291e72b4e0598df
Naman Soni <namantsoni@gmail.com>
2024-05-14 13:19:30 +0530

    Image Upload Functionality and Update User route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/userRoute.js';
 import authRouter from './routes/authRoute.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 //Routes 
 app.use("/api/user", userRouter);
