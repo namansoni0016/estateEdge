bbbd1c29d6dcb6a8cf00850e85c1d372f6405388
Naman Soni <namantsoni@gmail.com>
2024-05-19 11:11:47 +0530

    Delete user listing functionality

--- server/routes/listingRoute.js
+++ server/routes/listingRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createListing } from "../controllers/listingController.js";
+import { createListing, deleteListing } from "../controllers/listingController.js";
 import { verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post('/create', verifyUser, createListing);
+router.delete('/delete/:id', verifyUser, deleteListing);
 
 export default router;
