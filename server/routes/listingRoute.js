5c2bfd60dc3121ec6284318bdd2aa0c1cb801a82
Naman Soni <namantsoni@gmail.com>
2024-05-19 21:23:13 +0530

    Update listing page and functionality

--- server/routes/listingRoute.js
+++ server/routes/listingRoute.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { createListing, deleteListing } from "../controllers/listingController.js";
+import { 
+    createListing, 
+    deleteListing, 
+    updateListing, 
+    getListing } from "../controllers/listingController.js";
 import { verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post('/create', verifyUser, createListing);
 router.delete('/delete/:id', verifyUser, deleteListing);
+router.post('/update/:id', verifyUser, updateListing);
+router.get('/get/:id', getListing);
 
 export default router;
